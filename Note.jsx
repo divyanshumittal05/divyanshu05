2b8458b75953bcbcb151b54cd0fd34bb89b10c60
divyanshumittal05 <83198367+divyanshumittal05@users.noreply.github.com>
2021-06-06 14:06:52 +0530

    Add files via upload

---
+++ Note.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div classNane="note">
+      <h1>Javascript and React.js</h1>
+      <p>
+        This was an amazing bootcamp taken up by Shaurya Sir we covered
+        everything from scartch including Javascript and React.js, HTML.
+      </p>
+    </div>
+  );
+}
+export default Note;
